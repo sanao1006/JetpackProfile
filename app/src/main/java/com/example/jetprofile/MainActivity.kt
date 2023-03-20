815bfec0c0767830518c47400c6f1506e2955e09
sanao1006 <naosak1006@gmail.com>
2023-03-20 21:40:21 +0900

    fix

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -76,13 +80,8 @@ class MainActivity : ComponentActivity() {
                                 fontSize = 16.sp
                             )
                             Spacer(modifier = Modifier.height(16.dp))
+                            Label(icon = Icons.Default.Email, text = "Email")
 
-                            Row(verticalAlignment = Alignment.CenterVertically) {
-                                Icon(imageVector = Icons.Default.Email, contentDescription = "Email")
-
-                                Spacer(modifier = Modifier.width(10.dp))
-                                Text(text = "Email", fontSize = 14.sp, fontWeight = FontWeight.Bold)
-                            }
                             Spacer(modifier = Modifier.height(10.dp))
 
                             Text(text = "example@example.com", fontSize = 16.sp)
@@ -104,11 +103,45 @@ class MainActivity : ComponentActivity() {
                         ) {
                             Text(text = "詳細を表示", color = Color.White)
                         }
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Column(
+                            modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(10.dp))
+                                .background(Color.LightGray.copy(alpha = 0.3f))
+                                .padding(horizontal = 10.dp, vertical = 20.dp)
+                        ) {
+                            Label(
+                                icon = Icons.Default.Favorite,
+                                text = "趣味：散歩",
+                                color=Color.Gray
+                            )
+
+                            Spacer(modifier = Modifier.height(10.dp))
+
+                            Label(
+                                icon = Icons.Default.LocationOn,
+                                text = "居住地：東京",
+                                color = Color.Gray
+
+                            )
+
+                        }
                     }
 
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Label(icon: ImageVector, text: String, color: Color = MaterialTheme.colors.onBackground) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Icon(imageVector = icon, contentDescription = null)
+
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = text, fontSize = 14.sp, fontWeight = FontWeight.Bold, color = color)
     }
 }
 
